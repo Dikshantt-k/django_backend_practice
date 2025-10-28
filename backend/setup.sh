a0b27b76b34529ac7b1a42d9a81ff6e5bb3397dc
dikshant5 <dikshant@omnipayments.com>
2025-10-28 13:07:01 +0530

    change bash file

--- backend/setup.sh
+++ backend/setup.sh
@@ -4,3 +4,7 @@ pip install -r requirements.txt
 
 python manage.py makemigrations
 python manage.py migrate    
+
+echo "Current directory before collectstatic:"
+pwd
+python manage.py collectstatic
