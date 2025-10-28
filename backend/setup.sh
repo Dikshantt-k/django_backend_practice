ebb54938a38a4d1190978c7c9471d5bf83b0e87e
dikshant5 <dikshant@omnipayments.com>
2025-10-28 13:25:21 +0530

    minor changes in bash file

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,3 +1,14 @@
+set -x  # show commands
+echo ">>> Inside setup.sh" >&2
+
+echo ">>> Python version:" >&2
+python3 --version >&2
+
+echo ">>> Current directory before collectstatic:" >&2
+pwd >&2
+ls >&2
+
+
 pip install setuptools
 pip install -r requirements.txt
 
@@ -5,6 +16,5 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate    
 
-echo "Current directory before collectstatic:" >&2
-pwd >&2
+
 python manage.py collectstatic
